709c27e0d469ef44d229bb889a7ad251d91314e1
nitishSr <nsrivast@redhat.com>
2021-04-29 16:18:00 +0530

    Added modular and resuable function code to commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,28 +10,19 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+// Cypress.Commands.add("login", (email, password) => { ... })
 //
 //
 // -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
 //
 //
 // -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
 //
 //
 // -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-import '@testing-library/cypress/add-commands';
-import { userNameInput, userPasswordInput, LoginButton } from '../integration/views/login.view';
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-const tackle_ui_url = Cypress.env('tackleUrl');
-
-Cypress.Commands.add('login', (username, password) => {
-    cy.visit(tackle_ui_url)
-    cy.get(userNameInput).clear().type(username)
-    cy.get(userPasswordInput).clear().type(password)
-    cy.get(LoginButton).click()
-    cy.get('h1').contains('Application inventory')
-})
+import './controlsCommands.js';
+import './commonCommands.js';
---
+++ cypress/support/commonCommands.js
@@ -0,0 +1,13 @@
+import '@testing-library/cypress/add-commands';
+import { userNameInput, userPasswordInput, LoginButton } from '../integration/views/login.view';
+
+const tackleUiUrl = Cypress.env('tackleUrl');
+
+// -- Perform user login into Tackle UI --
+Cypress.Commands.add('login', (username, password) => {
+    cy.visit(tackleUiUrl);
+    cy.get(userNameInput).clear().type(username);
+    cy.get(userPasswordInput).clear().type(password);
+    cy.get(LoginButton).click();
+    cy.get('h1').contains('Application inventory');
+});
