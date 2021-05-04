2299e7b9b7a1300c04547906681d06a3fad22787
Shveta Sachdeva <sshveta@sshveta.remote.csb>
2021-05-03 22:04:12 -0700

    Jon Functions automation

---
+++ cypress/integration/tests/controls/jobfunctions/create.test.ts
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+import { login } from '../../../../utils/utils';
+
+describe('Create New Job Function', () => {
+
+    beforeEach(() => {
+        login();
+    });
+
+    it('Create job function', function() {
+        expect(true)
+    });
+
+   
+});
---
+++ cypress/utils/utils.ts
@@ -0,0 +1,25 @@
+import * as loginView from '../integration/views/login.view';
+
+const userName = Cypress.env('user');
+const userPassword = Cypress.env('pass');
+const tackleUiUrl = Cypress.env('tackleUrl');
+
+export function inputText(fieldId: string, text: string): void {
+  cy.get(fieldId).clear().type(text);
+}
+
+export function clickByText(fieldId: string, buttonText: string): void {
+  cy.contains(fieldId, buttonText).click();
+}
+
+export function click(fieldId: string): void {
+  cy.get(fieldId).click();
+}
+
+export function login(): void {
+  cy.visit(tackleUiUrl);
+  inputText(loginView.userNameInput, userName);
+  inputText(loginView.userPasswordInput, userPassword);
+  clickByText("button", loginView.loginButton);
+  cy.get('h1').contains('Application inventory');
+}
