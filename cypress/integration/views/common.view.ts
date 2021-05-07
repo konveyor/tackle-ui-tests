845d0ea086cc9760f198b930e97d340c111e595b
Ganesh Hubale <ghubale@redhat.com>
2021-05-07 17:27:15 +0530

    Updated with minor changes
    Signed-off-by: Ganesh Hubale <ghubale@redhat.com>

--- cypress/integration/views/common.view.ts
+++
@@ -1,1 +0,0 @@
-export const itemsPerPageSelector = 'button[aria-label="Items per page"]';
--- cypress/integration/views/stakeholdergroups.view.ts
+++ cypress/integration/views/stakeholdergroups.view.ts
@@ -1,8 +1,8 @@
 // Widgets on Stakeholder groups tab view
 export const createNewStakeholdergroupButton = "button[aria-label=create-stakeholder-group]";
-export const stakeholdergroupTable = 'table[aria-label="App table"]';
-export const stakeholdergroupTableRows = 'table[aria-label="App table"] > tbody > tr';
-export const itemsPerPageSelector = 'button[aria-label="Items per page"]';
+export const stakeholdergroupTable = "table[aria-label='App table']";
+export const stakeholdergroupTableRows = "table[aria-label='App table'] > tbody > tr";
+export const itemsPerPageSelector = "button[aria-label='Items per page']";
 
 // Widgets on Stakeholder groups delete dialog view
 export const confirmDeleteButton = "button[aria-label=confirm]";
@@ -10,6 +10,6 @@ export const confirmDeleteButton = "button[aria-label=confirm]";
 // Widgets on New stakeholder group view
 export const stakeholdergroupName = "input[name=name]";
 export const stakeholdergroupDescription = "textarea[name=description]";
-export const stakeholdergroupMember = '[placeholder="Select a memebr"]';
+export const stakeholdergroupMember = "[placeholder='Select a member']";
 export const stakeholdergroupCreateButton = "button[aria-label=submit]";
 export const stakeholdergroupCancelButton = "button[aria-label=cancel]";
