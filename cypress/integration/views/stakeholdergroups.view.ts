283b9ccdb3c3a30e083f2a61d05279d92f907522
Ganesh Hubale <ghubale@redhat.com>
2021-05-07 23:39:49 +0530

    Removed common widgets from stakeholder groups view
    Signed-off-by: Ganesh Hubale <ghubale@redhat.com>

--- cypress/integration/views/stakeholdergroups.view.ts
+++ cypress/integration/views/stakeholdergroups.view.ts
@@ -1,15 +1,3 @@
-// Widgets on Stakeholder groups tab view
-export const createNewStakeholdergroupButton = "button[aria-label=create-stakeholder-group]";
-export const stakeholdergroupTable = "table[aria-label='App table']";
-export const stakeholdergroupTableRows = "table[aria-label='App table'] > tbody > tr";
-export const itemsPerPageSelector = "button[aria-label='Items per page']";
-
-// Widgets on Stakeholder groups delete dialog view
-export const confirmDeleteButton = "button[aria-label=confirm]";
-
-// Widgets on New stakeholder group view
 export const stakeholdergroupName = "input[name=name]";
 export const stakeholdergroupDescription = "textarea[name=description]";
 export const stakeholdergroupMember = "[placeholder='Select a member']";
-export const stakeholdergroupCreateButton = "button[aria-label=submit]";
-export const stakeholdergroupCancelButton = "button[aria-label=cancel]";
