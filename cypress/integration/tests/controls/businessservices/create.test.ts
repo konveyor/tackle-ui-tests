8ea225e9e514ca55ab4b3511062647d7a9426a96
Ganesh Hubale <ghubale@redhat.com>
2021-05-17 18:39:56 +0530

    Automated test case - "Business services name and description contraints check"
    Signed-off-by: Ganesh Hubale <ghubale@redhat.com>

---
+++ cypress/integration/tests/controls/businessservices/create.test.ts
@@ -0,0 +1,53 @@
+/// <reference types="cypress" />
+
+import { login, clickByText, inputText, submitForm } from "../../../../utils/utils";
+import { navMenu, navTab } from "../../../views/menu.view";
+import {
+    controls,
+    businessservices,
+    button,
+    nameUniqueWarning,
+    nameLeastChars,
+    nameMaxChars,
+    descriptionMaxChars,
+    createNewButton,
+} from "../../../types/constants";
+import {
+    businessServiceNameInput,
+    businessServiceDescriptionInput,
+} from "../../../views/businessservice.view";
+import * as commonView from "../../../../integration/views/commoncontrols.view";
+
+import { BusinessServices } from "../../../models/businessservices";
+import * as faker from "faker";
+
+describe("Basic checks while creating business services", () => {
+    const businessServices = new BusinessServices();
+
+    beforeEach("Login", function () {
+        // Perform login
+        login();
+    });
+
+    it("Business services name and description contraints check", function () {
+        // Navigate to "New stakeholder group" page
+        clickByText(navMenu, controls);
+        clickByText(navTab, businessservices);
+        clickByText(button, createNewButton);
+        // Check "Create" and "Cancel" button status
+        cy.get(commonView.submitButton).should("be.disabled");
+        cy.get(commonView.cancelButton).should("not.be.disabled");
+
+        // Name constraints
+        inputText(businessServiceNameInput, "te");
+        cy.get(commonView.NameHelper).should("contain", nameLeastChars);
+        inputText(businessServiceNameInput, faker.random.words(50));
+        cy.get(commonView.NameHelper).should("contain", nameMaxChars);
+        inputText(businessServiceNameInput, "test");
+        cy.get(commonView.submitButton).should("not.be.disabled");
+
+        // Description constraints
+        inputText(businessServiceDescriptionInput, faker.random.words(120));
+        cy.get(commonView.DescriptionHelper).should("contain", descriptionMaxChars);
+    });
+});
