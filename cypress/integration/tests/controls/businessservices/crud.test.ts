de85210f9043f66bbc3f5bfa4fa5fa2b3ab69410
Ganesh Hubale <ghubale@redhat.com>
2021-05-19 18:32:06 +0530

    Updated crud test case Added interceptors
    Signed-off-by: Ganesh Hubale <ghubale@redhat.com>

--- cypress/integration/tests/controls/businessservices/crud.test.ts
+++ cypress/integration/tests/controls/businessservices/crud.test.ts
@@ -2,26 +2,36 @@
 
 import { login } from "../../../../utils/utils";
 import { BusinessServices } from "../../../models/businessservices";
+import * as data from "../../../../utils/data_utils";
 
 describe("A single Business service", () => {
-    const businessService = new BusinessServices();
+    const businessService = new BusinessServices(
+        data.getBusinessServiceName(),
+        data.getBusinessServiceDescription()
+    );
 
     beforeEach("Login", function () {
         // Perform login
         login();
+
+        // Interceptors
+        cy.intercept("POST", "/api/controls/business-service*").as("postBusinessService");
+        cy.intercept("GET", "/api/controls/business-service*").as("getBusinessService");
     });
 
     it("Business service crud operations", function () {
         // Create new Business service
         businessService.create();
-
-        businessService.exists();
+        cy.wait("@postBusinessService");
 
         // Edit Business service's name
-        businessService.edit({ name: businessService.getBusinessServiceName() });
+        var updatedBusinessServiceName = data.getBusinessServiceName();
+        businessService.edit({ name: updatedBusinessServiceName });
+        cy.wait("@getBusinessService");
 
         // Delete Business service
         businessService.delete();
+        cy.wait("@getBusinessService");
 
         // Assert that Business service is deleted
         businessService.notExists();
