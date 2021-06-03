9504ad22664d8a08d496f1e03e9d94a1f3bb22d1
Ganesh Hubale <ghubale@redhat.com>
2021-06-03 13:19:28 +0530

    Automated - Items per page validations
    Signed-off-by: Ganesh Hubale <ghubale@redhat.com>

--- cypress/integration/tests/controls/jobfunctions/pagination.test.ts
+++ cypress/integration/tests/controls/jobfunctions/pagination.test.ts
@@ -127,4 +127,29 @@ describe("Job functions pagination validations", function () {
         // Verify that navigation button to first page is enabled after moving to next page
         cy.get(firstPageButton).should("not.be.disabled");
     });
+
+    it("Items per page validations", function () {
+        // Navigate to Job functions tab
+        clickByText(navMenu, controls);
+        clickByText(navTab, jobfunctions);
+        cy.wait("@getJobfunctions");
+
+        // Select 10 items per page
+        selectItemsPerPage(10);
+        cy.wait(2000);
+
+        // Verify that only 10 items are displayed
+        cy.get("td[data-label=Name]").then(($rows) => {
+            cy.wrap($rows.length).should("eq", 10);
+        });
+
+        // Select 20 items per page
+        selectItemsPerPage(20);
+        cy.wait(2000);
+
+        // Verify that items less than or equal to 20 and greater than 10 are displayed
+        cy.get("td[data-label=Name]").then(($rows) => {
+            cy.wrap($rows.length).should("be.lte", 20).and("be.gt", 10);
+        });
+    });
 });
