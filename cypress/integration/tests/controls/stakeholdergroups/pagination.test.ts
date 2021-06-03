30c7878ebc1fe5acfb9b0434d9f48bc56f8e2d56
Ganesh Hubale <ghubale@redhat.com>
2021-06-03 11:53:29 +0530

    Automate pagination validation for stakeholder groups - Navigation button validations
    Signed-off-by: Ganesh Hubale <ghubale@redhat.com>

---
+++ cypress/integration/tests/controls/stakeholdergroups/pagination.test.ts
@@ -0,0 +1,133 @@
+/// <reference types="cypress" />
+
+import { login, clickByText, selectItemsPerPage, click } from "../../../../utils/utils";
+import { navMenu, navTab } from "../../../views/menu.view";
+import { controls, stakeholdergroups, tdTag, trTag } from "../../../types/constants";
+
+import { Stakeholdergroups } from "../../../models/stakeholdergroups";
+
+import * as data from "../../../../utils/data_utils";
+import {
+    firstPageButton,
+    lastPageButton,
+    nextPageButton,
+    pageNumInput,
+    prevPageButton,
+    appTable,
+    deleteButton,
+    confirmButton,
+} from "../../../views/common.view";
+
+var stakeholdergroupsList: Array<Stakeholdergroups> = [];
+
+describe("Stakeholder groups pagination validations", function () {
+    before("Login and Create Test Data", function () {
+        // Perform login
+        login();
+
+        // Navigate to stakeholder tab
+        clickByText(navMenu, controls);
+        clickByText(navTab, stakeholdergroups);
+        var rowsToCreate = 0;
+
+        // Get the current table row count and create appropriate test data rows
+        selectItemsPerPage(100);
+        cy.wait(2000);
+        cy.get(appTable)
+            .next()
+            .then(($div) => {
+                if (!$div.hasClass("pf-c-empty-state")) {
+                    cy.get("td[data-label=Name]").then(($rows) => {
+                        var rowCount = $rows.length;
+                        if (rowCount <= 10) {
+                            rowsToCreate = 11 - rowCount;
+                        }
+                        if (rowsToCreate > 0) {
+                            // Create multiple stakeholder groups
+                            for (let i = 0; i < rowsToCreate; i++) {
+                                const stakeholder = new Stakeholdergroups(
+                                    data.getEmail(),
+                                    data.getFullName()
+                                );
+                                stakeholder.create();
+                                stakeholdergroupsList.push(stakeholder);
+                            }
+                        }
+                    });
+                } else {
+                    rowsToCreate = 11;
+                    // Create multiple stakeholder groups
+                    for (let i = 0; i < rowsToCreate; i++) {
+                        const stakeholdergroup = new Stakeholdergroups(data.getFullName());
+                        stakeholdergroup.create();
+                        stakeholdergroupsList.push(stakeholdergroup);
+                    }
+                }
+            });
+    });
+
+    beforeEach("Persist session", function () {
+        // Save the session and token cookie for maintaining one login session
+        Cypress.Cookies.preserveOnce("AUTH_SESSION_ID", "KEYCLOAK_SESSION");
+
+        // Interceptors
+        cy.intercept("GET", "/api/controls/stakeholder-group*").as("getStakeholdergroups");
+    });
+
+    after("Perform test data clean up", function () {
+        // Delete the stakeholder groups created before the tests
+        if (stakeholdergroupsList.length > 0) {
+            // Navigate to stakeholder groups tab
+            clickByText(navMenu, controls);
+            clickByText(navTab, stakeholdergroups);
+            cy.wait("@getStakeholdergroups");
+            selectItemsPerPage(100);
+            cy.wait(2000);
+
+            stakeholdergroupsList.forEach(function (stakeholdergroup) {
+                cy.get("td[data-label=Name]").each(($rows) => {
+                    if ($rows.text() === stakeholdergroup.name) stakeholdergroup.delete();
+                });
+            });
+        }
+    });
+
+    it("Navigation button validations", function () {
+        // Navigate to stakeholder groups tab
+        clickByText(navMenu, controls);
+        clickByText(navTab, stakeholdergroups);
+        cy.wait("@getStakeholdergroups");
+
+        // select 10 items per page
+        selectItemsPerPage(10);
+        cy.wait("@getStakeholdergroups");
+
+        // Verify next buttons are enabled as there are more than 11 rows present
+        cy.get(nextPageButton).each(($nextBtn) => {
+            cy.wrap($nextBtn).should("not.be.disabled");
+        });
+
+        // Verify that previous buttons are disabled being on the first page
+        cy.get(prevPageButton).each(($previousBtn) => {
+            cy.wrap($previousBtn).should("be.disabled");
+        });
+
+        // Verify that navigation button to last page is enabled
+        cy.get(lastPageButton).should("not.be.disabled");
+
+        // Verify that navigation button to first page is disabled being on the first page
+        cy.get(firstPageButton).should("be.disabled");
+
+        // Navigate to next page
+        cy.get(nextPageButton).eq(0).click();
+        cy.wait("@getStakeholdergroups");
+
+        // Verify that previous buttons are enabled after moving to next page
+        cy.get(prevPageButton).each(($previousBtn) => {
+            cy.wrap($previousBtn).should("not.be.disabled");
+        });
+
+        // Verify that navigation button to first page is enabled after moving to next page
+        cy.get(firstPageButton).should("not.be.disabled");
+    });
+});
