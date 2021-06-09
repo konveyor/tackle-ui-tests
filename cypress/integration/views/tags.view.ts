bac1cedf181a1f8443a4865e48bb651f0fb5d135
nitishSr <nsrivast@redhat.com>
2021-06-09 14:48:20 +0530

    Added tag and tag type specific helper fields and validation error message constants

--- cypress/integration/views/tags.view.ts
+++ cypress/integration/views/tags.view.ts
@@ -1,7 +1,12 @@
 export const createTagButton = "Create tag";
 export const createTagtypeButton = "Create tag type";
+export const colorHelper = "div[id=color-helper]";
 export const dropdownMenuToggle = "button[aria-label='Options menu']";
+export const nameHelper = "div[id=name-helper]";
 export const nameInput = "input[aria-label='name']";
 export const rankInput = "input[aria-label='rank']";
+export const rankHelper = "div[id=rank-helper]";
 export const tagTable = "table[aria-label='tag-table']";
 export const tagMenuButton = "button[aria-label='Actions']";
+export const tagTypeHelper = "div[id=tagType-helper]";
+export const positiveRankMsg = "This field must be greater than 1.";
