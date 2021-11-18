9861334c5da2391e9a800c12631cb3560d531c30
Ganesh Hubale <ghubale@redhat.com>
2021-11-18 21:08:03 +0530

    Revert "[RFR] Create xml report file"

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -22,24 +22,24 @@
 // }
 // export a function
 
+const registerReportPortalPlugin = require("@reportportal/agent-js-cypress/lib/plugin");
+module.exports = (on) => registerReportPortalPlugin(on);
+
+module.exports = (on, config) => {
+    // configure plugins here
+    // Update report portal token value
+    const updatedConfig = {
+        ...config,
+        reporterOptions: {
+            ...config.reporterOptions,
+            token: process.env.RP_TOKEN,
+            endpoint: process.env.RP_ENDPOINT,
+        },
+    };
+    return updatedConfig;
+};
+
 module.exports = (on, config) => {
     // register cypress-grep plugin code
     require("cypress-grep/src/plugin")(config);
-};
-
-// To create consolidated report XML
-
-const { beforeRunHook, afterRunHook } = require("cypress-mochawesome-reporter/lib");
-const exec = require("child_process").execSync;
-module.exports = (on) => {
-    on("before:run", async (details) => {
-        console.log("override before:run");
-        await beforeRunHook(details);
-    });
-
-    on("after:run", async () => {
-        console.log("override after:run");
-        await exec("npx jrm ./cypress/reports/junitreport.xml ./cypress/reports/junit/*.xml");
-        await afterRunHook();
-    });
 };
