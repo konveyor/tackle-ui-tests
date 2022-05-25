a6fc2871edea3f577b7ddda3ff19760aea130080
amastbau <amastbau@redhat.com>
2022-05-25 18:07:48 +0300

    more packge config

--- cypress/integration/models/developer/controls/tags.ts
+++ cypress/integration/models/developer/controls/tags.ts
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import "cypress-react-selector";
 import { controls, tags, tdTag, trTag, button } from "../../../types/constants";
 import { navMenu, navTab } from "../../../views/menu.view";
 
