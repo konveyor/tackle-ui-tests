9ecc01a1c0fec79422c20f8d3b968d7f4896957e
Karishma Punwatkar <kpunwatk@redhat.com>
2022-07-12 15:52:11 +0530

    modified:   cypress/integration/tests/developer/controls/tags/tagtype/filter.test.ts 	modified:   cypress/utils/utils.ts
    [WIP] Test case for Tag type color filter validation
    
    	modified:   cypress.json
    	modified:   cypress/integration/tests/developer/controls/tags/tagtype/filter.test.ts
    	modified:   cypress/utils/utils.ts

--- cypress/integration/tests/developer/controls/tags/tagtype/filter.test.ts
+++ cypress/integration/tests/developer/controls/tags/tagtype/filter.test.ts
@@ -24,12 +24,20 @@ import {
     selectUserPerspective,
 } from "../../../../../../utils/utils";
 import { navMenu, navTab } from "../../../../../views/menu.view";
-import { controls, button, clearAllFilters, tags, tagType } from "../../../../../types/constants";
+import {
+    controls,
+    button,
+    clearAllFilters,
+    tags,
+    tagType,
+    color,
+} from "../../../../../types/constants";
 
 import * as data from "../../../../../../utils/data_utils";
+import { TagType } from "../../../../../models/developer/controls/tagtypes";
 
 describe("Tag type filter validations", { tags: "@tier2" }, function () {
-    before("Login", function () {
+    beforeEach("Login", function () {
         // Prevent hook from running, if the tag is excluded from run
         if (hasToBeSkipped("@tier2")) return;
 
@@ -64,6 +72,31 @@ describe("Tag type filter validations", { tags: "@tier2" }, function () {
 
         // Assert that no search results are found
         cy.get("h2").contains("No results found");
+
+        clickByText(button, clearAllFilters);
+    });
+
+    it("Tag type color filter validations", function () {
+        // Navigate to Tags tab
+        TagType.openList();
+
+        // Enter an existing tag type color substring and apply it as search filter
+        var validSearchInput = data.getColor();
+        applySearchFilter(color, validSearchInput);
+
+        // Assert that tag type row(s) containing the search text is/are displayed
+        exists(validSearchInput);
+
+        // Clear all filters
+        clickByText(button, clearAllFilters);
+        cy.get("@getTagtypes");
+
+        // Enter a non-existing tag type color substring and apply it as search filter
+        var invalidSearchInput = String(data.getRandomWord(3));
+        applySearchFilter(color, invalidSearchInput);
+
+        // Assert that no search results are found
+        cy.get("h2").contains("No tag types available");
 
         clickByText(button, clearAllFilters);
     });
