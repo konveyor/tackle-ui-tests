d3d9af921202a55eeca4b613ccd3b017cecc6f3a
Nandini Chandra <nachandr@redhat.com>
2022-10-24 16:10:17 -0500

    Update cyclic dependency test
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- cypress/integration/tests/developer/applicationinventory/assessment/dependency.test.ts
+++ cypress/integration/tests/developer/applicationinventory/assessment/dependency.test.ts
@@ -84,19 +84,19 @@ describe("Manage application dependencies", { tags: "@newtest" }, () => {
         // Add northbound and southbound dependencies for 2nd app from list
         applicationsList[1].addDependencies(northboundApps, southboundApps);
 
-        // Adding app 2 as northbound dependency for 1st app should yield cyclic error
+        // Adding app[2] as northbound dependency for app[0] should yield cyclic error
         applicationsList[0].openManageDependencies();
         applicationsList[0].selectDependency(northdependenciesDropdownBtn, [
-            applicationsList[1].name,
+            applicationsList[2].name,
         ]);
         cy.wait(500);
         cy.get(northboundHelper).should("contain.text", cyclicDependenciesErrorMsg);
         click(closeForm);
 
-        // Adding app 2 as southbound dependency for 3rd app should yield cyclic error
+        // Adding app[0] as southbound dependency for app[2] should yield cyclic error
         applicationsList[2].openManageDependencies();
         applicationsList[2].selectDependency(southdependenciesDropdownBtn, [
-            applicationsList[1].name,
+            applicationsList[0].name,
         ]);
         cy.wait(500);
         cy.get(southboundHelper).should("contain.text", cyclicDependenciesErrorMsg);
