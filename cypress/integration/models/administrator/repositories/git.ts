c448d75730b302e35b87c05394d45c0c1f54666a
midays <midays@redhat.com>
2022-11-20 16:12:23 +0200

    - changed comments

--- cypress/integration/models/administrator/repositories/git.ts
+++ cypress/integration/models/administrator/repositories/git.ts
@@ -5,28 +5,22 @@ import {
     uncheckInsecureRepository,
 } from "../../../../utils/utils";
 
-/**
- * static method used to navigate to the git configuration page
- */
 export class GitConfiguration {
     static open() {
+        // used to navigate to the git configuration page
         selectUserPerspective("Administrator");
         clickByText("a.pf-c-nav__link", "Git");
         cy.contains("h1", "Git configuration", { timeout: 5000 });
     }
 
-    /**
-     *  this functions used to navigate to the git configuration page under the administrator view and enable the insecure repo
-     */
     enableInsecureGitRepositories() {
+        // navigate to the git configuration page under the administrator view and enable the insecure repo
         GitConfiguration.open();
         checkInsecureRepository();
     }
 
-    /**
-     *  this functions used to navigate to the git configuration page under the administrator view and disable the insecure repo
-     */
     disableInsecureGitRepositories() {
+        // navigate to the git configuration page under the administrator view and disable the insecure repo
         GitConfiguration.open();
         uncheckInsecureRepository();
     }
--- cypress/integration/models/administrator/repositories/subversion.ts
+++ cypress/integration/models/administrator/repositories/subversion.ts
@@ -6,27 +6,22 @@ import {
 } from "../../../../utils/utils";
 
 export class SubversionConfiguration {
-    /**
-     * static method used to navigate to the subversion configuration page
-     */
+
     static open() {
+        // used to navigate to the subversion configuration page
         selectUserPerspective("Administrator");
         clickByText("a.pf-c-nav__link", "Subversion");
         cy.contains("h1", "Subversion configuration", { timeout: 5000 });
     }
 
-    /**
-     *  this functions used to navigate to the subversion configuration page under the administrator view and enable the insecure repo
-     */
     enableInsecureSubversionRepositories() {
+        // navigate to the subversion configuration page under the administrator view and enable the insecure repo
         SubversionConfiguration.open();
         checkInsecureRepository();
     }
 
-    /**
-     *  this functions used to navigate to the subversion configuration page under the administrator view and disable the insecure repo
-     */
     disableInsecureSubversionRepositories() {
+        // navigate to the subversion configuration page under the administrator view and disable the insecure repo
         SubversionConfiguration.open();
         uncheckInsecureRepository();
     }
