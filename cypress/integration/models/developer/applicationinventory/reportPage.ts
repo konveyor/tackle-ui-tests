ac56ecf4351b3c3c687967653032a0af8037bd86
Karishma Punwatkar <kpunwatk@redhat.com>
2022-12-12 19:23:38 +0530

    Signed-off-by: Karishma Punwatkar <kpunwatk@redhat.com>
    [RFR] Sort test cases for report page
    	modified:   cypress/fixtures/analysis.json
    	modified:   cypress/integration/models/developer/applicationinventory/analysis.ts
    	new file:   cypress/integration/tests/reports/sort.test.ts
    
    	modified:   cypress/fixtures/analysis.json
    	modified:   cypress/integration/models/developer/applicationinventory/analysis.ts
    	new file:   cypress/integration/tests/reports/sort.test.ts
    
    	modified:   cypress/fixtures/analysis.json
    	new file:   cypress/integration/models/developer/applicationinventory/reportPage.ts
    	new file:   cypress/integration/tests/reports/sort.test.ts
    	new file:   cypress/integration/views/reportPage.view.ts

---
+++ cypress/integration/models/developer/applicationinventory/reportPage.ts
@@ -0,0 +1,44 @@
+import { SEC } from "../../../types/constants";
+import { clickByText } from "../../../../utils/utils";
+import {
+    dropdownMenu,
+    selectFilter,
+    inputTextField,
+    selectSortBy,
+} from "../../../views/reportPage.view";
+
+export class Report {
+    applyFilter(filterName: string, searchText: string): void {
+        // Select the filter by filterName
+        cy.get(selectFilter).within(() => {
+            cy.get("div.input-group-btn").eq(0).click();
+            cy.get(dropdownMenu).within(() => {
+                clickByText("a", filterName);
+            });
+        });
+        // Enter the search text and Enter
+        cy.get(inputTextField).eq(0).clear().type(searchText).type("{enter}");
+        cy.wait(2 * SEC);
+    }
+
+    applySortAction(sortbySelector: string) {
+        // Select the sort-by options
+        cy.get(selectSortBy).click();
+        cy.get(dropdownMenu).within(() => {
+            clickByText("a", sortbySelector);
+        });
+    }
+
+    getTableColumnData(itemList: any[]): Array<string> {
+        cy.get(".real", { timeout: 5 * SEC })
+            .not(".pf-c-table__expandable-row")
+            .each(($ele) => {
+                if ($ele.text() !== "") {
+                    itemList.push(Number($ele.text()));
+                } else {
+                    itemList.push($ele.text().toString().toLowerCase());
+                }
+            });
+        return itemList;
+    }
+}
