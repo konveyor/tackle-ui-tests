15097a5ec257e909fd0af5e3c8870e9454cb70ed
midays <midays@redhat.com>
2023-02-07 15:54:41 +0200

    check if clear repo button is disabled before moving with the test

--- cypress/integration/models/administrator/repositories/maven.ts
+++ cypress/integration/models/administrator/repositories/maven.ts
@@ -27,10 +27,18 @@ export class MavenConfiguration {
     clearRepository() {
         MavenConfiguration.open();
         cy.wait(2000);
-        clickByText(clearRepository, "Clear repository");
 
-        cy.get(".pf-c-modal-box__title-text").contains("Clear repository");
+        cy.get("#clear-repository")
+            .invoke("attr", "aria-disabled")
+            .then((disabled) => {
+                cy.log(disabled);
+                if (disabled == "false") {
+                    clickByText(clearRepository, "Clear repository");
 
-        click(confirmClear);
+                    cy.get(".pf-c-modal-box__title-text").contains("Clear repository");
+
+                    click(confirmClear);
+                }
+            });
     }
 }
