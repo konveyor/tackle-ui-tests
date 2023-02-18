716969ba468e1fbc3b653edd6afaf7664f393b61
Nandini Chandra <nachandr@redhat.com>
2023-02-17 18:39:30 -0600

    Updated after block
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- cypress/integration/tests/developer/applicationinventory/assessment/assessment_after_import.ts
+++ cypress/integration/tests/developer/applicationinventory/assessment/assessment_after_import.ts
@@ -21,26 +21,24 @@ import {
     exists,
     importApplication,
     login,
-    openManageImportsPage,
-    openErrorReport,
-    verifyAppImport,
-    verifyImportErrorMsg,
     deleteApplicationTableRows,
-    preservecookies,
     hasToBeSkipped,
     selectUserPerspective,
-    deleteAllBusinessServices,
     deleteAppImportsTableRows,
     notExists,
 } from "../../../../../utils/utils";
+
+import * as data from "../../../../../utils/data_utils";
 import { BusinessServices } from "../../../../models/developer/controls/businessservices";
 import { navMenu } from "../../../../views/menu.view";
 import { applicationInventory, button, SEC } from "../../../../types/constants";
+import { Stakeholders } from "../../../../models/developer/controls/stakeholders";
 import { Assessment } from "../../../../models/developer/applicationinventory/assessment";
 
 const businessService = new BusinessServices("BS_tag_test");
 const filePath = "app_import/csv/";
 var applicationsList: Array<Assessment> = [];
+const stakeholdersList: Array<Stakeholders> = [];
 const stakeholdersNameList: Array<string> = [];
 
 describe("Operations after application import", () => {
@@ -51,18 +49,17 @@ describe("Operations after application import", () => {
         // Perform login
         login();
 
+        // Navigate to stakeholders control tab and create new stakeholder
+        const stakeholder = new Stakeholders(data.getEmail(), data.getFullName());
+        stakeholder.create();
+        cy.wait(2000);
+
+        stakeholdersList.push(stakeholder);
+        stakeholdersNameList.push(stakeholder.name);
+
         // Delete the existing application rows
-        // deleteApplicationTableRows();
-        // deleteAllBusinessServices();
-    });
-
-    beforeEach("Persist session", function () {
-        // Save the session and token cookie for maintaining one login session
-        preservecookies();
-
-        // Interceptors
-        cy.intercept("GET", "/hub/application*").as("getApplication");
-        // deleteAppImportsTableRows();
+        deleteApplicationTableRows();
+        deleteAppImportsTableRows();
     });
 
     after("Perform test data clean up", function () {
@@ -70,41 +67,43 @@ describe("Operations after application import", () => {
         if (hasToBeSkipped("@tier1") && hasToBeSkipped("@newtest")) return;
 
         // Delete the existing application rows before deleting business service(s)
-        clickByText(navMenu, applicationInventory);
         deleteApplicationTableRows();
+        deleteAppImportsTableRows();
     });
 
-    it("Perform assessment after a successful application import", { tags: "@tier1" }, function () {
-        selectUserPerspective("Developer");
-        clickByText(navMenu, applicationInventory);
-        cy.wait(2000);
-        // cy.wait("@getApplication");
+    it(
+        "Perform assessment and review after a successful application import",
+        { tags: "@tier1" },
+        function () {
+            selectUserPerspective("Developer");
+            clickByText(navMenu, applicationInventory);
+            cy.wait(2000);
 
-        /*
-        // Import valid csv
-        const fileName = "template_application_import.csv";
-        importApplication(filePath + fileName);
-        cy.wait(2000);
+            // Import valid csv
+            const fileName = "template_application_import.csv";
+            importApplication(filePath + fileName);
+            cy.wait(2000);
 
-        // Verify imported apps are visible in table
-        exists("Customers");
-        exists("Inventory");
-        exists("Gateway"); */
+            // Verify imported apps are visible in table
+            exists("Customers");
+            exists("Inventory");
+            exists("Gateway");
 
-        const application = new Assessment(getRandomApplicationData("Customers", true));
-        // Perform assessment of application
-        application.perform_assessment("low", stakeholdersNameList);
-        cy.wait(2000);
-        application.verifyStatus("assessment", "Completed");
+            const application = new Assessment(getRandomApplicationData("Customers", true));
+            // Perform assessment of application
+            application.perform_assessment("low", stakeholdersNameList);
+            cy.wait(2000);
+            application.verifyStatus("assessment", "Completed");
 
-        // Perform application review
-        application.perform_review("low");
-        cy.wait(2000);
-        application.verifyStatus("review", "Completed");
+            // Perform application review
+            application.perform_review("low");
+            cy.wait(2000);
+            application.verifyStatus("review", "Completed");
 
-        // Delete application
-        application.delete();
-        cy.wait(2000);
-    });
-}
-)
+            // Delete application
+            application.delete();
+            cy.wait(2000);
+            notExists(application.name);
+        }
+    );
+});
