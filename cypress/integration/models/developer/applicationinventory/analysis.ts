4ed6b3bdc83fc2d07521fe0de4fd24499c87e591
kpunwatk <kpunwatk@redhat.com>
2023-03-15 21:53:09 +0530

    [RFR] Fixes for Stakeholder's sort test and Stakeholdergroup's filter test (#395)
    Signed-off-by: Karishma Punwatkar <kpunwatk@redhat.com>
    
    	modified:   cypress/integration/tests/developer/controls/stakeholdergroups/filter.test.ts
    	modified:   cypress/integration/tests/developer/controls/stakeholders/sort.test.ts
    
    	modified:   cypress/integration/models/developer/applicationinventory/analysis.ts
    	modified:   cypress/integration/views/common.view.ts

--- cypress/integration/models/developer/applicationinventory/analysis.ts
+++ cypress/integration/models/developer/applicationinventory/analysis.ts
@@ -29,6 +29,7 @@ import {
 import { navMenu, navTab } from "../../../views/menu.view";
 import {
     cancelForm,
+    checkSuccessAlert,
     click,
     clickByText,
     clickWithin,
@@ -65,6 +66,7 @@ import {
     tabsPanel,
 } from "../../../views/analysis.view";
 import { kebabMenu } from "../../../views/applicationinventory.view";
+import * as commonView from "../../../views/common.view";
 import { AnalysisStatuses } from "../../../types/constants";
 
 export class Analysis extends Application {
@@ -233,8 +235,7 @@ export class Analysis extends Application {
             if (this.enableTransaction) this.enableTransactionAnalysis();
             if (!this.sources) cy.contains("button", "Next", { timeout: 200 }).click();
             cy.contains("button", "Run", { timeout: 200 }).click();
-            // checkSuccessAlert(commonView.successAlertMessage, `Submitted for analysis`);
-            // Commented the line because of the BZ https://issues.redhat.com/browse/TACKLE-890
+            checkSuccessAlert(commonView.infoAlertMessage, `Submitted for analysis`);
         }
     }
 
