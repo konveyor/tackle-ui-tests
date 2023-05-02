1d4b90a2b925e3b75416f2d73e19874473c0a60c
Igor Braginsky <ibragins@redhat.com>
2023-05-02 22:45:00 +0300

    Removed old files
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>

--- cypress/integration/views/repository.view.ts
+++
@@ -1,5 +0,0 @@
-export const clearRepository = "#clear-repository";
-export const confirmClear = 'button[aria-label="confirm"]';
-export const gitSecure = "#git";
-export const subversionSecure = "#svn";
-export const mavenSecure = "#maven-secure";
