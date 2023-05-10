8cdaf0c24b8a74cd62f64d5ea1c536ae84cdf204
Nandini Chandra <nachandr@redhat.com>
2023-05-09 19:04:16 -0500

    Update clickWithin() utility function
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- cypress/utils/utils.ts
+++ cypress/utils/utils.ts
@@ -1229,9 +1229,11 @@ export function selectWithinModal(selector: string): void {
 }
 
 export function clickWithin(parent, selector: string): void {
-    cy.get(parent, { timeout: 30 * SEC }).within(() => {
-        click(selector);
-    });
+    cy.get(parent, { timeout: 30 * SEC })
+        .eq(0)
+        .within(() => {
+            click(selector);
+        });
 }
 
 //function to select checkboxes
