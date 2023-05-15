7021f4a5d8063c4f7407402ce426332ac20cc020
Shveta <sshveta@redhat.com>
2023-05-15 14:50:43 -0700

    RBAC session fix
    Signed-off-by: Shveta <sshveta@redhat.com>

--- cypress/e2e/models/keycloak/users/user.ts
+++ cypress/e2e/models/keycloak/users/user.ts
@@ -39,13 +39,17 @@ export class User {
         cy.visit(User.keycloakUrl, { timeout: 120 * SEC });
         cy.contains("h1", "Welcome to", { timeout: 120 * SEC }); // Make sure that welcome page opened and loaded
         clickByText("a", "Administration Console");
-
         // This is required to be skipped if admin user is logged in already to keycloak
         if (!loggedIn) {
-            cy.get("#kc-header-wrapper", { timeout: 120 * SEC }); // Make sure that login page opened and loaded
-            inputText(loginView.userNameInput, "admin");
-            inputText(loginView.userPasswordInput, keycloakAdminPassword);
-            click(loginView.loginButton);
+            cy.get("h1").then(($isloggedIn) => {
+                // Due to session sometimes console auto logs in, hence this check is necessary
+                if ($isloggedIn.text().toString().trim() === "Sign in to your account") {
+                    cy.get("#kc-header-wrapper", { timeout: 240 * SEC }); // Make sure that login page opened and loaded
+                    inputText(loginView.userNameInput, "admin");
+                    inputText(loginView.userPasswordInput, keycloakAdminPassword);
+                    click(loginView.loginButton);
+                }
+            });
         }
     }
 
