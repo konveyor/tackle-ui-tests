cc89ff80df363acfdd56c891283af9aeffc1f52d
Neilhamza <nhamza@redhat.com>
2023-08-13 15:52:08 +0300

    adapted more selector
    Signed-off-by: Neilhamza <nhamza@redhat.com>

--- cypress/e2e/views/migration-wave.view.ts
+++ cypress/e2e/views/migration-wave.view.ts
@@ -22,7 +22,7 @@ export enum MigrationWaveView {
     waveTable = "table[aria-label='Migration waves table']",
     createTrackerButton = "#create-tracker",
     waveExpanded = "pf-m-expanded",
-    removeApplicationButton = ".pf-u-text-align-right > button",
+    removeApplicationButton = ".pf-v5-u-text-align-right > button",
 }
 
 export enum MigrationWavesSpecialColumns {
