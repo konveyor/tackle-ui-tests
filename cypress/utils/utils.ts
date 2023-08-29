4c068d8492fbf04973706c85063325d8baa7777d
Igor Braginsky <ibragins@redhat.com>
2023-08-29 19:00:28 +0300

    [RFR] Added clickWithFocus() and clickJs() functions (#700)
    * Added clickWithFocus() and clickJs() functions
    
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>
    
    * Changed timeout to 30 sec instead of 60 for click functions
    
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>
    
    ---------
    
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>

--- cypress/utils/utils.ts
+++ cypress/utils/utils.ts
@@ -126,7 +126,25 @@ export function click(fieldId: string, isForced = true, log = false): void {
     if (!log) {
         cy.log(`Click ${fieldId}`);
     }
-    cy.get(fieldId, { log, timeout: 60 * SEC }).click({ log, force: isForced });
+    cy.get(fieldId, { log, timeout: 30 * SEC }).click({ log, force: isForced });
+}
+
+export function clickWithFocus(fieldId: string, isForced = true, log = false): void {
+    if (!log) {
+        cy.log(`Click ${fieldId}`);
+    }
+    cy.get(fieldId, { log, timeout: 30 * SEC })
+        .focus()
+        .click({ log, force: isForced });
+}
+
+export function clickJs(fieldId: string, isForced = true, log = false): void {
+    if (!log) {
+        cy.log(`Click ${fieldId}`);
+    }
+    cy.get(fieldId, { log, timeout: 30 * SEC }).then(($obj) => {
+        $obj[0].click();
+    });
 }
 
 export function submitForm(): void {
