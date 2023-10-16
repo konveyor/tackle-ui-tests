2f9d7b4890e47b9719b6a1f9da2f18d0075c730c
Igor Braginsky <ibragins@redhat.com>
2023-10-16 19:27:22 +0300

    [RFR] Initial commit for issues functionality (#736)
    * Initial commit for issues functionality
    
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>
    
    * Added filtering methods
    
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>
    
    * Moved selectors to view file
    
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>
    
    * Added more filters
    
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>
    
    * Added one more app to fixtures
    
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>
    
    * Refactored filtering to use same method
    
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>
    
    * Refactored conditions to make code more generic and flexible
    
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>
    
    ---------
    
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>

---
+++ cypress/e2e/models/migration/dynamicreports/issue.ts
@@ -0,0 +1,62 @@
+import {
+    click,
+    clickByText,
+    clickWithinByText,
+    getUrl,
+    inputText,
+    selectFilter,
+    selectItemsPerPage,
+    selectUserPerspective,
+} from "../../../../utils/utils";
+import { button, filterIssue, migration, SEC } from "../../../types/constants";
+import { navMenu } from "../../../views/menu.view";
+import { searchButton, span } from "../../../views/common.view";
+import {
+    appFilterName,
+    bsFilterName,
+    categoryFilterName,
+    sourceFilterName,
+    tagFilterName,
+    targetFilterName,
+} from "../../../views/issue.view";
+
+export class Issue {
+    /** Contains URL of issues web page */
+    static fullUrl = Cypress.env("tackleUrl") + "/issues";
+
+    public static openList(itemsPerPage = 100, forceReload = false): void {
+        if (forceReload) {
+            cy.visit(Issue.fullUrl);
+        }
+        if (!getUrl().includes(Issue.fullUrl)) {
+            selectUserPerspective(migration);
+        }
+        clickByText(navMenu, "Issues");
+        cy.wait(2 * SEC);
+        selectItemsPerPage(itemsPerPage);
+    }
+
+    public static filterBy(item: string, itemName: string | string[]): void {
+        //TODO: Refactor this after bug https://issues.redhat.com/browse/MTA-1465 will be fixed
+        const selectorMap: Record<string, string> = {
+            [filterIssue.appName]: appFilterName,
+            [filterIssue.category]: categoryFilterName,
+            [filterIssue.source]: sourceFilterName,
+            [filterIssue.target]: targetFilterName,
+        };
+
+        selectFilter(item);
+        if (selectorMap[item]) {
+            inputText(selectorMap[item], itemName);
+            click(searchButton);
+        } else if (item == filterIssue.bs && !Array.isArray(itemName)) {
+            click(bsFilterName);
+            clickWithinByText(bsFilterName, button, itemName);
+        } else if (item == filterIssue.tags && Array.isArray(itemName)) {
+            click(tagFilterName);
+            itemName.forEach((name) => {
+                clickWithinByText(tagFilterName, span, name);
+            });
+        }
+    }
+}
