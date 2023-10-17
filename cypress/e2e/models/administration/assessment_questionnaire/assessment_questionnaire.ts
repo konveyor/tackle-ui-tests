fc47224f4d3e86bbd2bbded93f007a88e9b59594
Nandini Chandra <nachandr@redhat.com>
2023-10-17 18:54:44 -0500

    Add methods to delete and export questionnaires
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- cypress/e2e/models/administration/assessment_questionnaire/assessment_questionnaire.ts
+++ cypress/e2e/models/administration/assessment_questionnaire/assessment_questionnaire.ts
@@ -1,9 +1,13 @@
-import { click, clickByText, selectUserPerspective } from "../../../../utils/utils";
-import { SEC, assessmentQuestionnaires } from "../../../types/constants";
+import { click, clickByText, selectUserPerspective, inputText, performRowActionByIcon } from "../../../../utils/utils";
+import { SEC, assessmentQuestionnaires, deleteAction } from "../../../types/constants";
 import { legacyPathfinderToggle } from "../../../views/assessmentquestionnaire.view";
 import { navMenu } from "../../../views/menu.view";
 import { button } from "../../../../e2e/types/constants";
+import { actionButton } from "../../../views/applicationinventory.view";
 
+"input#required-switch-0";
+
+// const fileName = "questionnaire_import/cloud-native.yaml"
 export interface AssessmentQuestionnaire {
     fileName: string;
 }
@@ -11,11 +15,12 @@ export interface AssessmentQuestionnaire {
 export class AssessmentQuestionnaire {
     public static fullUrl = Cypress.env("tackleUrl") + "/assessment";
 
+    /*
     constructor(
         fileName: string,
     ) {
         this.fileName = fileName;
-    }
+    }*/
 
     public static open() {
         cy.url().then(($url) => {
@@ -26,16 +31,38 @@ export class AssessmentQuestionnaire {
         });
     }
 
-    public importQuestionnaire() {
+    public static importQuestionnaire(fileName) {
         AssessmentQuestionnaire.open();
         clickByText(button, "Import questionnaire");
-        cy.get('input[type="file"]', { timeout: 2 * SEC }).attachFile(fileName, {
+        cy.get('input#yamlFile-file-upload-filename', { timeout: 2 * SEC }).attachFile(fileName, {
             subjectType: "drag-n-drop",
         });
         cy.get("form.pf-v5-c-form", { timeout: 5 * SEC })
         .find("button")
         .contains("Import")
-        .trigger("click");
+        .click();
+    }
+
+    public static deleteQuestionnaire(fileName) {
+        AssessmentQuestionnaire.open();
+        cy.contains(fileName, { timeout: 120 * SEC })
+        .closest('tr')
+        .within(() => {
+            click(actionButton);
+        })
+        clickByText(button, deleteAction);
+        inputText(".confirm-deletion-input", fileName);
+        clickByText(button, deleteAction);
+    }
+
+    public static exportQuestionnaire(fileName) {
+        AssessmentQuestionnaire.open();
+        cy.contains(fileName, { timeout: 120 * SEC })
+        .closest('tr')
+        .within(() => {
+            click(actionButton);
+        })
+        clickByText(button, 'Export');
     }
 
     public downloadYamlTemplate() {
