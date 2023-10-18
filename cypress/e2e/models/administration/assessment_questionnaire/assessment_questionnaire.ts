0697f7bbc59c082b7a914d7580f64db6fa5b2e3d
Nandini Chandra <nachandr@redhat.com>
2023-10-18 16:22:47 -0500

    Moved selectors to view file
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- cypress/e2e/models/administration/assessment_questionnaire/assessment_questionnaire.ts
+++ cypress/e2e/models/administration/assessment_questionnaire/assessment_questionnaire.ts
@@ -1,26 +1,18 @@
-import { click, clickByText, selectUserPerspective, inputText, performRowActionByIcon } from "../../../../utils/utils";
+import { click, clickByText, selectUserPerspective } from "../../../../utils/utils";
 import { SEC, assessmentQuestionnaires, deleteAction } from "../../../types/constants";
-import { legacyPathfinderToggle } from "../../../views/assessmentquestionnaire.view";
+import {
+    legacyPathfinderToggle,
+    questionnaireUpload,
+    confirmDeletion,
+    importQuestionnaire,
+} from "../../../views/assessmentquestionnaire.view";
 import { navMenu } from "../../../views/menu.view";
 import { button } from "../../../../e2e/types/constants";
 import { actionButton } from "../../../views/applicationinventory.view";
-
-"input#required-switch-0";
-
-// const fileName = "questionnaire_import/cloud-native.yaml"
-export interface AssessmentQuestionnaire {
-    fileName: string;
-}
+import { controlsForm } from "../../../views/common.view";
 
 export class AssessmentQuestionnaire {
     public static fullUrl = Cypress.env("tackleUrl") + "/assessment";
-
-    /*
-    constructor(
-        fileName: string,
-    ) {
-        this.fileName = fileName;
-    }*/
 
     public static open() {
         cy.url().then(($url) => {
@@ -33,59 +25,52 @@ export class AssessmentQuestionnaire {
 
     public static importQuestionnaire(fileName) {
         AssessmentQuestionnaire.open();
-        clickByText(button, "Import questionnaire");
-        cy.get('input#yamlFile-file-upload-filename', { timeout: 2 * SEC }).attachFile(fileName, {
+        click(importQuestionnaire);
+        cy.get(questionnaireUpload, { timeout: 2 * SEC }).attachFile(fileName, {
             subjectType: "drag-n-drop",
         });
-        cy.get("form.pf-v5-c-form", { timeout: 5 * SEC })
-        .find("button")
-        .contains("Import")
-        .click();
+        cy.get(controlsForm, { timeout: 5 * SEC })
+            .find("button")
+            .contains("Import")
+            .click();
     }
 
     public static deleteQuestionnaire(fileName) {
         AssessmentQuestionnaire.open();
         cy.contains(fileName, { timeout: 120 * SEC })
-        .closest('tr')
-        .within(() => {
-            click(actionButton);
-        })
+            .closest("tr")
+            .within(() => {
+                click(actionButton);
+            });
         clickByText(button, deleteAction);
-        inputText(".confirm-deletion-input", fileName);
+        cy.get(confirmDeletion).click().focused().clear().type(fileName);
         clickByText(button, deleteAction);
     }
 
     public static exportQuestionnaire(fileName) {
         AssessmentQuestionnaire.open();
         cy.contains(fileName, { timeout: 120 * SEC })
-        .closest('tr')
-        .within(() => {
-            click(actionButton);
-        })
-        clickByText(button, 'Export');
+            .closest("tr")
+            .within(() => {
+                click(actionButton);
+            });
+        clickByText(button, "Export");
     }
 
-    public static toggleQuestionnaire(fileName, toggle: boolean) {
+    public static viewQuestionnaire(fileName) {
         AssessmentQuestionnaire.open();
-        let selector  = (toggle)? ".pf-m-on" : ".pf-m-off";
         cy.contains(fileName, { timeout: 120 * SEC })
             .closest("tr")
             .within(() => {
-                cy.get(selector)
-                    .invoke("css", "display")
-                    .then((display) => {
-                        if (display.toString() == "none") {
-                            click(".pf-v5-c-switch__toggle");
-                        }
-                    });
+                click(actionButton);
             });
+        clickByText(button, "View");
     }
 
     public downloadYamlTemplate() {
         AssessmentQuestionnaire.open();
     }
 
-    /*
     public static enableLegacyQuestionanire() {
         AssessmentQuestionnaire.open();
         cy.get(legacyPathfinderToggle, { timeout: 2 * SEC }).then(($checkbox) => {
@@ -102,5 +87,5 @@ export class AssessmentQuestionnaire {
                 click(legacyPathfinderToggle);
             }
         });
-    }*/
+    }
 }
--- cypress/e2e/views/assessmentquestionnaire.view.ts
+++ cypress/e2e/views/assessmentquestionnaire.view.ts
@@ -1,1 +1,4 @@
 export const legacyPathfinderToggle = "input#required-switch-0";
+export const questionnaireUpload = "input#yamlFile-file-upload-filename";
+export const confirmDeletion = ".confirm-deletion-input";
+export const importQuestionnaire = "#import-questionnaire";
