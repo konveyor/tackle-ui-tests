2ec169b3dc375277691e8cfbea6f7c5c444056ee
Nandini Chandra <nachandr@redhat.com>
2023-10-19 10:56:15 -0500

    [RFR] Add methods for questionnaire class (#749)
    * Add method for importing questionnaire
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Add methods to delete and export questionnaires
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Add methods to toggle questionnaire status
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Moved selectors to view file
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Add operation method
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    ---------
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- cypress/e2e/models/administration/assessment_questionnaire/assessment_questionnaire.ts
+++ cypress/e2e/models/administration/assessment_questionnaire/assessment_questionnaire.ts
@@ -1,7 +1,15 @@
 import { click, clickByText, selectUserPerspective } from "../../../../utils/utils";
-import { SEC, assessmentQuestionnaires } from "../../../types/constants";
-import { legacyPathfinderToggle } from "../../../views/assessmentquestionnaire.view";
+import { SEC, assessmentQuestionnaires, deleteAction } from "../../../types/constants";
+import {
+    legacyPathfinderToggle,
+    questionnaireUpload,
+    confirmDeletion,
+    importQuestionnaire,
+} from "../../../views/assessmentquestionnaire.view";
 import { navMenu } from "../../../views/menu.view";
+import { button } from "../../../../e2e/types/constants";
+import { actionButton } from "../../../views/applicationinventory.view";
+import { controlsForm } from "../../../views/common.view";
 
 export class AssessmentQuestionnaire {
     public static fullUrl = Cypress.env("tackleUrl") + "/assessment";
@@ -15,8 +23,40 @@ export class AssessmentQuestionnaire {
         });
     }
 
-    public importQuestionnaire() {
+    public static operation(fileName, operation) {
         AssessmentQuestionnaire.open();
+        cy.contains(fileName, { timeout: 120 * SEC })
+            .closest("tr")
+            .within(() => {
+                click(actionButton);
+            });
+        clickByText(button, operation);
+    }
+
+    public static importQuestionnaire(fileName) {
+        AssessmentQuestionnaire.open();
+        click(importQuestionnaire);
+        cy.get(questionnaireUpload, { timeout: 2 * SEC }).attachFile(fileName, {
+            subjectType: "drag-n-drop",
+        });
+        cy.get(controlsForm, { timeout: 5 * SEC })
+            .find("button")
+            .contains("Import")
+            .click();
+    }
+
+    public static deleteQuestionnaire(fileName) {
+        AssessmentQuestionnaire.operation(fileName, deleteAction);
+        cy.get(confirmDeletion).click().focused().clear().type(fileName);
+        clickByText(button, deleteAction);
+    }
+
+    public static exportQuestionnaire(fileName) {
+        AssessmentQuestionnaire.operation(fileName, "Export");
+    }
+
+    public static viewQuestionnaire(fileName) {
+        AssessmentQuestionnaire.operation(fileName, "View");
     }
 
     public downloadYamlTemplate() {
