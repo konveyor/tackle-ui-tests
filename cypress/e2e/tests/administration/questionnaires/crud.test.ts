2924672644263016f9759f7066f7045c574e4a07
Nandini Chandra <nachandr@redhat.com>
2023-10-26 18:40:42 -0500

    [RFR] Questionnaire CRUD operations (#769)
    * Questionnaire CRUD operations
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Questionnaire CRUD operations
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Updated selector name
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Add questionnaire YAML file
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Add questionnaire
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * A few updates
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    ---------
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

---
+++ cypress/e2e/tests/administration/questionnaires/crud.test.ts
@@ -0,0 +1,37 @@
+import { AssessmentQuestionnaire } from "../../../models/administration/assessment_questionnaire/assessment_questionnaire";
+import { checkSuccessAlert, login, closeModalWindow } from "../../../../utils/utils";
+import { alertTitle } from "../../../views/common.view";
+
+const yamlFileName = "questionnaire_import/cloud-native.yaml";
+const fileName = "Cloud Native";
+
+describe(["@tier2"], "Questionnaire CRUD operations", () => {
+    before("Login", function () {
+        login();
+    });
+
+    it("Import questionnaire", function () {
+        AssessmentQuestionnaire.import(yamlFileName);
+        checkSuccessAlert(
+            alertTitle,
+            `Success alert:Questionnaire ${fileName} was successfully created.`,
+            true
+        );
+        AssessmentQuestionnaire.enable(fileName, false);
+    });
+
+    it("Duplicate questionnaire Test", function () {
+        AssessmentQuestionnaire.import(yamlFileName);
+        checkSuccessAlert(alertTitle, "UNIQUE constraint failed: Questionnaire.Name");
+        closeModalWindow();
+    });
+
+    it("Delete questionnaire", function () {
+        AssessmentQuestionnaire.delete(fileName);
+        checkSuccessAlert(
+            alertTitle,
+            `Success alert:Questionnaire ${fileName} was successfully deleted.`,
+            true
+        );
+    });
+});
