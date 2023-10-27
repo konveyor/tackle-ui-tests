214eb2e1f18423888be662068cbfa1b39a7e3509
Nandini Chandra <nachandr@redhat.com>
2023-10-27 16:58:04 -0500

    Download questionnaire template
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

---
+++ cypress/e2e/tests/administration/questionnaires/miscalleneous.test.ts
@@ -0,0 +1,18 @@
+import { AssessmentQuestionnaire } from "../../../models/administration/assessment_questionnaire/assessment_questionnaire";
+import { click, login } from "../../../../utils/utils";
+import { downloadYamlTemplate } from "../../../views/assessmentquestionnaire.view";
+
+const yamlFileName = "questionnaire_import/cloud-native.yaml";
+const fileName = "Cloud Native";
+
+describe(["@tier2"], "Questionnaire CRUD operations", () => {
+    before("Login", function () {
+        login();
+    });
+
+    it("Download YAML template", function () {
+        AssessmentQuestionnaire.open();
+        click(downloadYamlTemplate);
+        cy.readFile("cypress/downloads/questionnaire-template.yaml").should("contain", "Test questionnaire  (SAMPLE)");
+    })
+});
