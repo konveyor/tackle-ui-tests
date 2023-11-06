74617d9200b20cea4574bcde72180c6ac9c8bb33
Nandini Chandra <nachandr@redhat.com>
2023-11-06 11:03:43 -0600

    [RFR] Update discard_assessment() method (#786)
    * Updated discard_assessment() method
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Updated discard_assessment() method
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Updated discard_assessment() method
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    ---------
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

---
+++ cypress/e2e/tests/migration/applicationinventory/assessment/miscellaneous.test.ts
@@ -0,0 +1,74 @@
+/*
+Copyright © 2021 the Konveyor Contributors (https://konveyor.io/)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+/// <reference types="cypress" />
+
+import {
+    login,
+    createMultipleApplications,
+    deleteByList,
+    checkSuccessAlert,
+} from "../../../../../utils/utils";
+
+import * as data from "../../../../../utils/data_utils";
+import { Stakeholders } from "../../../../models/migration/controls/stakeholders";
+import { Assessment } from "../../../../models/migration/applicationinventory/assessment";
+import { AssessmentQuestionnaire } from "../../../../models/administration/assessment_questionnaire/assessment_questionnaire";
+import { alertTitle } from "../../../../views/common.view";
+
+const fileName = "Legacy Pathfinder";
+let stakeholderList: Array<Stakeholders> = [];
+let stakeholderNameList: Array<string> = [];
+let applicationList: Array<Assessment> = [];
+
+describe(["@tier3"], "Tests related to application assessment and review", () => {
+    before("Perform application assessment and review", function () {
+        login();
+        cy.intercept("GET", "/hub/application*").as("getApplication");
+
+        AssessmentQuestionnaire.enable(fileName);
+
+        const stakeholder = new Stakeholders(data.getEmail(), data.getFullName());
+        stakeholder.create();
+        cy.wait(2000);
+        stakeholderList.push(stakeholder);
+        stakeholderNameList.push(stakeholder.name);
+
+        applicationList = createMultipleApplications(1);
+        applicationList[0].perform_assessment("low", stakeholderNameList);
+        cy.wait(2000);
+        applicationList[0].verifyStatus("assessment", "Completed");
+        applicationList[0].perform_review("low");
+        cy.wait(2000);
+        applicationList[0].verifyStatus("review", "Completed");
+    });
+
+    it("Discard Assess/Review", function () {
+        applicationList[0].discard_assessment();
+        /* Add this after bug MTA-1611 is resolved.
+        checkSuccessAlert(alertTitle, `Success alert:Success! Assessment discarded for applicationList[0].name.`);
+        applicationList[0].verifyStatus("assessment", "Not started"); */
+        checkSuccessAlert(
+            alertTitle,
+            `Success alert:Success! Review discarded for ${applicationList[0].name}.`
+        );
+        applicationList[0].verifyStatus("review", "Not started");
+    });
+
+    after("Perform test data clean up", function () {
+        deleteByList(stakeholderList);
+        deleteByList(applicationList);
+    });
+});
