163fa9a5b08c59822d9d1dd7b6f732a095573e76
midays <92722851+midays@users.noreply.github.com>
2023-11-30 01:58:21 +0200

    [RFR][TA] MTA-392 --- View archived assessments (#841)
    * Signed-off-by: mohamed idays midays@redhat.com
    initial commit
    
    * Signed-off-by: mohamed idays midays@redhat.com
    tweaks
    
    * Signed-off-by: mohamed idays midays@redhat.com
    tweaks
    
    * Signed-off-by: mohamed idays midays@redhat.com
    tweaks
    
    * Signed-off-by: mohamed idays midays@redhat.com
    rename test
    
    * Signed-off-by: mohamed idays midays@redhat.com
    fixed formatting

--- cypress/e2e/views/assessmentquestionnaire.view.ts
+++ cypress/e2e/views/assessmentquestionnaire.view.ts
@@ -4,3 +4,5 @@ export const confirmDeletion = "#confirm-deletion-input";
 export const importQuestionnaire = "#import-questionnaire";
 export const switchToggle = ".pf-v5-c-switch__toggle";
 export const downloadYamlTemplate = "#download-yaml-template";
+export const ArchivedQuestionnaires = "Archived questionnaires";
+export const ArchivedQuestionnairesTableDataCell = 'td[data-label="Archived questionnaires"]';
