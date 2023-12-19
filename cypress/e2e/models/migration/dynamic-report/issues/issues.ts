cb84bde7e94c22b5a03c8cccc17efc8e4959ad65
Igor Braginsky <ibragins@redhat.com>
2023-12-19 14:26:01 +0200

    Added detailed issue values inspection (#885)
    * Added initial commit for ispecting detailed issue values
    
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>
    
    * Added some validations to unfolded issues
    
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>
    
    * Finalised issue detailed fields validation
    
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>
    
    ---------
    
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>

--- cypress/e2e/models/migration/dynamic-report/issues/issues.ts
+++ cypress/e2e/models/migration/dynamic-report/issues/issues.ts
@@ -16,6 +16,7 @@ import {
     migration,
     SEC,
     singleApplication,
+    tdTag,
     trTag,
 } from "../../../../types/constants";
 import { navMenu } from "../../../../views/menu.view";
@@ -28,7 +29,7 @@ import {
     tagFilterName,
 } from "../../../../views/issue.view";
 import { AppIssue } from "../../../../types/types";
-import { liTag, searchButton, searchInput, span } from "../../../../views/common.view";
+import { div, liTag, searchButton, searchInput, span } from "../../../../views/common.view";
 
 export class Issues {
     /** Contains URL of issues web page */
@@ -106,5 +107,52 @@ export class Issues {
             clickByText(span, filterValue);
             click(selector);
         }
+    }
+
+    public static unfold(name: string) {
+        cy.contains(tdTag, name)
+            .closest(trTag)
+            .within(() => {
+                cy.get("[id^=expandable]").then(($button) => {
+                    if (!$button.hasClass("pf-m-expanded")) {
+                        $button.trigger("click");
+                    }
+                });
+            });
+    }
+
+    public static validateAllFields(issue: AppIssue): void {
+        const sections = {
+            totalAffectedApps: "Total affected",
+            targetTechnologies: "Target technologies",
+            sourceTechnologies: "Source technologies",
+            ruleSet: "Rule set",
+            rule: /^Rule$/,
+            labels: "Labels",
+        };
+
+        Issues.unfold(issue.name);
+        Issues.validateSection(sections.totalAffectedApps, button, /\d - View affected /);
+        Issues.validateSection(sections.targetTechnologies, span, issue.targets);
+        Issues.validateSection(sections.sourceTechnologies, div, issue.source);
+        Issues.validateSection(sections.ruleSet, div, issue.ruleSet);
+        Issues.validateSection(sections.rule, div, issue.rule);
+        Issues.validateSection(sections.labels, span, issue.labels);
+    }
+
+    private static validateSection(
+        title: string | RegExp,
+        contentSelector: string,
+        content: string | string[] | RegExp
+    ): void {
+        cy.contains("h4", title)
+            .next("div")
+            .within(() => {
+                if (Array.isArray(content)) {
+                    content.forEach((item) => cy.contains(contentSelector, item));
+                } else {
+                    cy.contains(contentSelector, content);
+                }
+            });
     }
 }
