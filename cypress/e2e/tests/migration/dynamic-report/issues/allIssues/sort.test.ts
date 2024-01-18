e360b638a6318106457586cbe3329d9b1c5ec7d2
Igor Braginsky <ibragins@redhat.com>
2024-01-18 11:51:40 +0200

    Added sorting by category
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>

--- cypress/e2e/tests/migration/dynamic-report/issues/allIssues/sort.test.ts
+++ cypress/e2e/tests/migration/dynamic-report/issues/allIssues/sort.test.ts
@@ -27,27 +27,27 @@ describe(["@tier2"], "Issues sort validations", function () {
         cy.intercept("GET", "/hub/application*").as("getApplication");
     });
 
-    it(
-        ["@tier2"],
-        "BUG MTA-2067 - Source Analysis on daytrader app and its issues sorting validation",
-        function () {
-            // Create Analysis instance in before hook
-            application = new Analysis(
-                getRandomApplicationData("daytrader-app", {
-                    sourceData: this.appData["daytrader-app"],
-                }),
-                getRandomAnalysisData(this.analysisData["source+dep_analysis_on_daytrader-app"])
-            );
-            application.create();
-            applicationsList.push(application);
-            cy.wait("@getApplication");
-            application.analyze();
-            cy.wait(2 * SEC);
-            application.verifyAnalysisStatus("Completed");
-            Issues.openList(10);
-            validateSortBy("Issue");
-        }
-    );
+    it("BUG MTA-2067 - Source Analysis on daytrader app and its issues sorting validation", function () {
+        // Create Analysis instance in before hook
+        application = new Analysis(
+            getRandomApplicationData("daytrader-app", {
+                sourceData: this.appData["daytrader-app"],
+            }),
+            getRandomAnalysisData(this.analysisData["source+dep_analysis_on_daytrader-app"])
+        );
+        application.create();
+        applicationsList.push(application);
+        cy.wait("@getApplication");
+        application.analyze();
+        cy.wait(2 * SEC);
+        application.verifyAnalysisStatus("Completed");
+        Issues.openList();
+        validateSortBy("Issue");
+    });
+    it("Sort issues by category", function () {
+        Issues.openList();
+        validateSortBy("Category");
+    });
 
     after("Perform test data clean up", function () {
         Analysis.open(true);
