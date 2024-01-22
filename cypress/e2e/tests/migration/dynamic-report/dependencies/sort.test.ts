2586ed8e89dc2f7b97d7716c92a5bec37ff2b6b4
Igor Braginsky <ibragins@redhat.com>
2024-01-22 17:27:53 +0200

    Added sorting test for dependencies
    Signed-off-by: Igor Braginsky <ibragins@redhat.com>

---
+++ cypress/e2e/tests/migration/dynamic-report/dependencies/sort.test.ts
@@ -0,0 +1,55 @@
+import { Analysis } from "../../../../models/migration/applicationinventory/analysis";
+import {
+    getRandomAnalysisData,
+    getRandomApplicationData,
+    login,
+    validateSortBy,
+} from "../../../../../utils/utils";
+import { SEC } from "../../../../types/constants";
+import { Dependencies } from "../../../../models/migration/dynamic-report/dependencies/dependencies";
+
+describe(["@tier2"], "Dependencies sort validations", function () {
+    let application: Analysis;
+    const sortByList = ["Dependency name", "Labels", "Found in"];
+
+    before("Load data", function () {
+        login();
+
+        cy.intercept("GET", "/hub/application*").as("getApplication");
+    });
+
+    beforeEach("Test title", function () {
+        cy.fixture("application").then(function (appData) {
+            this.appData = appData;
+        });
+        cy.fixture("analysis").then(function (analysisData) {
+            this.analysisData = analysisData;
+        });
+    });
+
+    it("Creating data for sorting", function () {
+        application = new Analysis(
+            getRandomApplicationData("daytrader-app", {
+                sourceData: this.appData["daytrader-app"],
+            }),
+            getRandomAnalysisData(this.analysisData["source+dep_analysis_on_daytrader-app"])
+        );
+        application.create();
+        cy.wait("@getApplication");
+        application.analyze();
+        cy.wait(2 * SEC);
+        application.verifyAnalysisStatus("Completed");
+    });
+
+    sortByList.forEach((column) => {
+        it(`Sort dependencies by ${column}`, function () {
+            Dependencies.openList();
+            validateSortBy(column);
+        });
+    });
+
+    after("Perform test data clean up", function () {
+        Analysis.open(true);
+        application.delete();
+    });
+});
