292b5555a89952ffb630161f95e0423470a0cbc0
kpunwatk <kpunwatk@redhat.com>
2024-02-09 00:54:39 +0530

    minor change (#949)
    Signed-off-by: Karishma Punwatkar <kpunwatk@redhat.com>

--- cypress/e2e/tests/migration/applicationinventory/analysis/affected_files.test.ts
+++ cypress/e2e/tests/migration/applicationinventory/analysis/affected_files.test.ts
@@ -22,14 +22,35 @@ import {
     deleteByList,
 } from "../../../../../utils/utils";
 import { Analysis } from "../../../../models/migration/applicationinventory/analysis";
-import { SEC } from "../../../../types/constants";
+import { CredentialType, SEC, UserCredentials } from "../../../../types/constants";
 import { Application } from "../../../../models/migration/applicationinventory/application";
 import { AppIssue } from "../../../../types/types";
+import { CredentialsSourceControlUsername } from "../../../../models/administration/credentials/credentialsSourceControlUsername";
+import { CredentialsMaven } from "../../../../models/administration/credentials/credentialsMaven";
+import * as data from "../../../../../utils/data_utils";
 let applicationsList: Array<Analysis> = [];
+let source_credential: CredentialsSourceControlUsername;
+let maven_credential: CredentialsMaven;
 
 describe(["@tier2"], "Bug MTA-2006: Affected files validation", () => {
     before("Login", function () {
         login();
+
+        // Create source Credentials
+        source_credential = new CredentialsSourceControlUsername(
+            data.getRandomCredentialsData(
+                CredentialType.sourceControl,
+                UserCredentials.usernamePassword,
+                true
+            )
+        );
+        source_credential.create();
+
+        // Create Maven credentials
+        maven_credential = new CredentialsMaven(
+            data.getRandomCredentialsData(CredentialType.maven, null, true)
+        );
+        maven_credential.create();
     });
 
     beforeEach("Load data", function () {
@@ -65,6 +86,75 @@ describe(["@tier2"], "Bug MTA-2006: Affected files validation", () => {
         application.verifyAnalysisStatus("Completed");
         application.validateIssues(this.analysisData["affected_files_on_day_trader_app"]["issues"]);
         this.analysisData["affected_files_on_day_trader_app"]["issues"].forEach(
+            (currentIssue: AppIssue) => {
+                application.validateAffected(currentIssue);
+            }
+        );
+    });
+
+    it("Affected files validation with source analysis on bookserver app", function () {
+        // Automate bug https://issues.redhat.com/browse/MTA-1628
+        const application = new Analysis(
+            getRandomApplicationData("affected_files_on_bookserverapp", {
+                sourceData: this.appData["bookserver-app"],
+            }),
+            getRandomAnalysisData(this.analysisData["affected_files_on_bookserverapp"])
+        );
+        application.create();
+        applicationsList.push(application);
+        cy.wait("@getApplication");
+        cy.wait(2 * SEC);
+        application.analyze();
+        application.verifyAnalysisStatus("Completed");
+        application.validateIssues(this.analysisData["affected_files_on_bookserverapp"]["issues"]);
+        this.analysisData["affected_files_on_bookserverapp"]["issues"].forEach(
+            (currentIssue: AppIssue) => {
+                application.validateAffected(currentIssue);
+            }
+        );
+    });
+
+    it("Affected files validation with source analysis on tackle-testapp app", function () {
+        // Automate bug https://issues.redhat.com/browse/MTA-1600
+        const application = new Analysis(
+            getRandomApplicationData("affected_files_on_tackleTestapp", {
+                sourceData: this.appData["tackle-testapp-git"],
+            }),
+            getRandomAnalysisData(this.analysisData["affected_files_on_tackleTestapp"])
+        );
+        application.create();
+        applicationsList.push(application);
+        cy.wait("@getApplication");
+        cy.wait(2 * SEC);
+        application.manageCredentials(source_credential.name, maven_credential.name);
+        application.analyze();
+        application.verifyAnalysisStatus("Completed");
+        application.validateIssues(this.analysisData["affected_files_on_tackleTestapp"]["issues"]);
+        this.analysisData["affected_files_on_tackleTestapp"]["issues"].forEach(
+            (currentIssue: AppIssue) => {
+                application.validateAffected(currentIssue);
+            }
+        );
+    });
+
+    it("Affected files validation with source+deps analysis on tackle-testapp app", function () {
+        // Automate bug https://issues.redhat.com/browse/MTA-1622
+        const application = new Analysis(
+            getRandomApplicationData("affected_files_on_tackleTestapp_deps", {
+                sourceData: this.appData["tackle-testapp-git"],
+            }),
+            getRandomAnalysisData(this.analysisData["affected_files_on_tackleTestapp_deps"])
+        );
+        application.create();
+        applicationsList.push(application);
+        cy.wait("@getApplication");
+        cy.wait(2 * SEC);
+        application.analyze();
+        application.verifyAnalysisStatus("Completed");
+        application.validateIssues(
+            this.analysisData["affected_files_on_tackleTestapp_deps"]["issues"]
+        );
+        this.analysisData["affected_files_on_tackleTestapp_deps"]["issues"].forEach(
             (currentIssue: AppIssue) => {
                 application.validateAffected(currentIssue);
             }
