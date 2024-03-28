083036c0fcf88ac8fcc31e76096b6f5a7001154b
Nandini Chandra <nachandr@redhat.com>
2024-03-27 21:27:51 -0500

    Added additional tests
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- cypress/e2e/tests/rbac/disable_keycloak.test.ts
+++ cypress/e2e/tests/rbac/disable_keycloak.test.ts
@@ -22,8 +22,14 @@ import {
 } from "../../../utils/utils";
 import { Stakeholders } from "../../models/migration/controls/stakeholders";
 import { Application } from "../../models/migration/applicationinventory/application";
-import { legacyPathfinder } from "../../types/constants";
+import {
+    legacyPathfinder,
+    cloudReadinessQuestionnaire,
+    cloudReadinessFilePath,
+    SEC,
+} from "../../types/constants";
 import { AssessmentQuestionnaire } from "../../models/administration/assessment_questionnaire/assessment_questionnaire";
+import { Analysis } from "../../models/migration/applicationinventory/analysis";
 
 let application = new Application(getRandomApplicationData());
 let stakeholders: Stakeholders[];
@@ -33,10 +39,11 @@ describe(["@tier5"], "Perform certain operations after disabling Keycloak", func
     before("Disable Keycloak", function () {
         login();
         patchTackleCR("keycloak", false);
-
         application.create();
         AssessmentQuestionnaire.deleteAllQuestionnaires();
-        AssessmentQuestionnaire.enable(legacyPathfinder);
+        AssessmentQuestionnaire.import(cloudReadinessFilePath);
+        AssessmentQuestionnaire.enable(cloudReadinessQuestionnaire);
+        AssessmentQuestionnaire.disable(legacyPathfinder);
 
         stakeholders = createMultipleStakeholders(1);
     });
@@ -48,19 +55,26 @@ describe(["@tier5"], "Perform certain operations after disabling Keycloak", func
         });
     });
 
-    it("With Auth disabled, Perform application assessment", function () {
-        application.perform_assessment("low", stakeholders);
+    it("With Auth disabled, Perform application assessment and review", function () {
+        application.perform_assessment("high", stakeholders, null, cloudReadinessQuestionnaire);
         cy.wait(1000);
+        application.verifyStatus("assessment", "Completed");
+
+        application.perform_review("low");
+        cy.wait(SEC);
         application.verifyStatus("assessment", "Completed");
     });
 
-    it("With Auth disabled, verify presence of Review application button", function () {
-        application.validateReviewButton(this.rbacRules);
+    it("Validate content of top kebab menu", function () {
+        // Import button should be available
+        Analysis.validateTopActionMenu(this.rbacRules);
+    });
+
+    it("Validate content of application kebab menu", function () {
+        application.validateAppContextMenu(this.rbacRules);
     });
 
     after("Clean up", function () {
-        login();
-
         application.delete();
         deleteByList(stakeholders);
     });
