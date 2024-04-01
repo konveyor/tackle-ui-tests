66e197e00122bf6c44b1a94fad804d3e93cf363c
Neilhamza <132557033+Neilhamza@users.noreply.github.com>
2024-04-02 00:11:31 +0300

    [RFR] assessment - miscellaneous mark two bugs (#1046)
    Signed-off-by: nhamza <nhamza@redhat.com>
    Signed-off-by: Shveta Sachdeva <sshveta@redhat.com>
    Co-authored-by: Shveta Sachdeva <sshveta@redhat.com>

--- cypress/e2e/tests/migration/applicationinventory/assessment/miscellaneous.test.ts
+++ cypress/e2e/tests/migration/applicationinventory/assessment/miscellaneous.test.ts
@@ -283,7 +283,7 @@ describe(["@tier3"], "Tests related to application assessment and review", () =>
         deleteByList(tags);
     });
 
-    it("Deletes assessments from archived questionnaire associated with an archetype and an application", function () {
+    it("BUG MTA-2505: Deletes assessments from archived questionnaire associated with an archetype and an application", function () {
         //automates polarion MTA-441 and MTA-442
         const applications = createMultipleApplications(1);
         const archetypes = createMultipleArchetypes(1);
