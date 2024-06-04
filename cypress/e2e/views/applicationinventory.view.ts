e2005c97950aec816a23e593dd75a6b748d2522f
Nandini Chandra <nachandr@redhat.com>
2024-06-04 17:20:45 -0500

    Update toolbar kebab menu selector
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- cypress/e2e/views/applicationinventory.view.ts
+++ cypress/e2e/views/applicationinventory.view.ts
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-export const actionButton = "button[aria-label='kebab dropdown toggle']";
+export const actionButton = "#toolbar-kebab";
 export const applicationsActionButton = "button[aria-label='Application actions']";
 
 export const applicationNameInput = "input[name=name]";
