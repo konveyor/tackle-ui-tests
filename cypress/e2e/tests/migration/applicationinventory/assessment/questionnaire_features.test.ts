7d06d6d78cea15baf533e1f6a3bdcfc0e1458556
Nandini Chandra <nachandr@redhat.com>
2024-08-13 17:02:28 -0500

    Mark bug for application questionnaire test
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- cypress/e2e/tests/migration/applicationinventory/assessment/questionnaire_features.test.ts
+++ cypress/e2e/tests/migration/applicationinventory/assessment/questionnaire_features.test.ts
@@ -97,7 +97,7 @@ describe(["@tier3"], "Tests for application questionnaire features", () => {
         Assessment.verifyButtonEnabled("Take");
     });
 
-    it("Bug: MTA-3417 1) Test auto answer feature of questionnaires; 2) Save assessment", function () {
+    it("Bug MTA-3417: 1) Test auto answer feature of questionnaires; 2) Save assessment", function () {
         //Automates Polarion MTA-388: Auto answer
         Application.open();
         cy.wait(2 * SEC);
