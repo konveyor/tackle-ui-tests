649a9374a2e1339be89639399a18f643d74e4385
Shveta Sachdeva <sshveta@redhat.com>
2025-04-01 14:39:58 -0700

    Modify github workflows to accept baseUrl
    Signed-off-by: Shveta Sachdeva <sshveta@redhat.com>

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -53,11 +53,3 @@ Cypress.Commands.add(
     }
 );
 
-Cypress.Commands.overwrite("log", (log, message, ...args) => {
-    // print the to Cypress Command Log
-    // to preserve the existing functionality
-    log(message, ...args);
-    // send the formatted message down to the Node
-    // callback in the cypress.config.js to be printed to the terminal
-    cy.task("print", [message, ...args].join(", "), { log: false });
-});
