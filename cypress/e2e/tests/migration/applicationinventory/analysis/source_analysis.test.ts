c36d8827f639ea8c246a167d6cb4d549388b2544
Shveta Sachdeva <sshveta@redhat.com>
2025-05-13 14:23:53 -0700

    review assessment test (#1496)
    Signed-off-by: Shveta Sachdeva <sshveta@redhat.com>

--- cypress/e2e/tests/migration/applicationinventory/analysis/source_analysis.test.ts
+++ cypress/e2e/tests/migration/applicationinventory/analysis/source_analysis.test.ts
@@ -107,7 +107,7 @@ describe(["@tier2"], "Source Analysis", () => {
         );
     });
 
-    it("Source + dependencies analysis on daytrader app", function () {
+    it("Bug MTA-5179: Source + dependencies analysis on daytrader app", function () {
         // Automate bug https://issues.redhat.com/browse/TACKLE-721
         const application = new Analysis(
             getRandomApplicationData("dayTraderApp_Source+dependencies", {
