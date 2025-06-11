1d032fb290b87103d72010707758821c77a9e3ae
Nandini Chandra <nachandr@redhat.com>
2025-06-10 23:59:46 -0500

    Task Manager drawer test
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

---
+++ cypress/e2e/tests/migration/task-manager/task_manager_drawer.test.ts
@@ -0,0 +1,64 @@
+/*
+Copyright © 2021 the Konveyor Contributors (https://konveyor.io/)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+/// <reference types="cypress" />
+
+import {
+    deleteByList,
+    getRandomAnalysisData,
+    getRandomApplicationData,
+    login,
+} from "../../../../utils/utils";
+import { Analysis } from "../../../models/migration/applicationinventory/analysis";
+import { taskNotificationBadge } from "../../../views/common.view";
+import { AnalysisStatuses } from "../../../types/constants";
+
+const analyses: Analysis[] = [];
+const NUMBER_OF_APPS = 2;
+
+describe(["@tier3"], "Task drawer validation", () => {
+    before("Login", function () {
+        login();
+        cy.visit("/");
+        cy.fixture("application").then((appData) => {
+            cy.fixture("analysis").then((analysisData) => {
+                for (let i = 0; i < NUMBER_OF_APPS; i++) {
+                    analyses.push(
+                        new Analysis(
+                            getRandomApplicationData("bookserverApp", {
+                                sourceData: appData["bookserver-app"],
+                            }),
+                            getRandomAnalysisData(analysisData["analysis_for_openSourceLibraries"])
+                        )
+                    );
+                }
+
+                analyses.forEach((analysis) => analysis.create());
+            });
+        });
+    });
+
+    it("Perform bulk analysis and validate information on task drawer", function () {
+        Analysis.analyzeAll(analyses[0]);
+        cy.get(taskNotificationBadge).click();
+        // cy.get("h2.list-item-header-title").index(0).contains("(analyzer) - app1 -10");
+        cy.get('*[class^="list-item-header-title"]').eq(0).contains("(analyzer) - app1 -10");
+    });
+
+    after("Perform test data clean up", function () {
+        Analysis.open(true);
+        deleteByList(analyses);
+    });
+});
