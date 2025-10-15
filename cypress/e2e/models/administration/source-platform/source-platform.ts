f39c1ddad114acade83148dfe44a7b965bd5e0ba
Nandini Chandra <nachandr@redhat.com>
2025-10-14 22:08:51 -0500

    [RFR] Model Source platform page (#1695)
    * Model Source platform page
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Add CF crud tests
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Update getRandomCredentialsData() function
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Add CF creation & deletion tests
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Add Source platform view file
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Update cypress.config.ts
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Address CodeRabbit review comments
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    * Address review comments
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>
    
    ---------
    
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

---
+++ cypress/e2e/models/administration/source-platform/source-platform.ts
@@ -0,0 +1,108 @@
+/*
+Copyright © 2021 the Konveyor Contributors (https://konveyor.io/)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+import {
+    cancelForm,
+    click,
+    clickByText,
+    clickItemInKebabMenu,
+    inputText,
+    selectFormItems,
+    selectItemsPerPage,
+    selectUserPerspective,
+    submitForm,
+} from "../../../../utils/utils";
+import { administration, SEC } from "../../../types/constants";
+import * as commonView from "../../../views/common.view";
+import { navMenu } from "../../../views/menu.view";
+import * as sourcePlatform from "../../../views/source-platform.view";
+
+export class SourcePlatform {
+    name: string;
+    type: string;
+    url: string;
+    credentials?: string;
+
+    constructor(name: string, type: string, url: string, credentials?: string) {
+        this.name = name;
+        this.type = type;
+        this.url = url;
+        this.credentials = credentials;
+    }
+
+    static fullUrl = Cypress.config("baseUrl") + "/source-platforms";
+
+    public static open(forceReload = false) {
+        const itemsPerPage = 100;
+        if (forceReload) {
+            cy.visit(SourcePlatform.fullUrl, { timeout: 15 * SEC }).then((_) => {
+                cy.get("h1", { timeout: 35 * SEC }).should("contain", "Source platforms");
+                selectItemsPerPage(itemsPerPage);
+            });
+            return;
+        }
+
+        cy.url().then(($url) => {
+            if ($url !== SourcePlatform.fullUrl) {
+                selectUserPerspective(administration);
+                clickByText(navMenu, "Source platforms");
+                cy.get("h1", { timeout: 60 * SEC }).should("contain", "Source platforms");
+            }
+        });
+        selectItemsPerPage(itemsPerPage);
+    }
+
+    protected fillName(name: string): void {
+        inputText(sourcePlatform.name, name);
+    }
+
+    protected selectType(type: string): void {
+        selectFormItems(sourcePlatform.type, type);
+    }
+
+    protected fillUrl(url: string): void {
+        inputText(sourcePlatform.url, url);
+    }
+
+    protected selectCredentials(credentials: string): void {
+        selectFormItems(sourcePlatform.credentials, credentials);
+    }
+
+    create(cancel = false): void {
+        SourcePlatform.open();
+        cy.contains("button", "Create new platform", { timeout: 20000 })
+            .should("be.visible")
+            .and("not.be.disabled")
+            .click();
+        if (cancel) {
+            cancelForm();
+        } else {
+            this.fillName(this.name);
+            this.selectType(this.type);
+            this.fillUrl(this.url);
+            if (this.credentials) this.selectCredentials(this.credentials);
+            submitForm();
+        }
+    }
+
+    // TODO: Add update method and corresponding test
+    delete(cancel = false): void {
+        SourcePlatform.open();
+        clickItemInKebabMenu(this.name, "Delete");
+        if (cancel) {
+            cancelForm();
+        } else click(commonView.confirmButton);
+    }
+}
---
+++ cypress/e2e/tests/administration/source-platform/crud.test.ts
@@ -0,0 +1,75 @@
+/*
+Copyright © 2021 the Konveyor Contributors (https://konveyor.io/)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+/// <reference types="cypress" />
+
+import * as data from "../../../../utils/data_utils";
+import { checkSuccessAlert, exists, login, notExists } from "../../../../utils/utils";
+import { CredentialsSourceControlUsername } from "../../../models/administration/credentials/credentialsSourceControlUsername";
+import { SourcePlatform } from "../../../models/administration/source-platform/source-platform";
+import { CredentialType, UserCredentials } from "../../../types/constants";
+import { successAlertMessage } from "../../../views/common.view";
+
+let cloudFoundryCreds: CredentialsSourceControlUsername;
+
+describe(["@tier2"], "CRUD operations on Cloud Foundry Source platform", () => {
+    before("Login", function () {
+        if (
+            !Cypress.env("cloudfoundry_user") ||
+            !Cypress.env("cloudfoundry_password") ||
+            !Cypress.env("cloudfoundry_url")
+        ) {
+            throw new Error(`One or more required Cloud Foundry env variables are missing in cypress.config.ts :
+            \ncloudfoundry_user\ncloudfoundry_password\ncloudfoundry_url`);
+        }
+        login();
+        cy.visit("/");
+        cloudFoundryCreds = new CredentialsSourceControlUsername(
+            data.getRandomCredentialsData(
+                CredentialType.sourceControl,
+                UserCredentials.usernamePassword,
+                false,
+                null,
+                null,
+                true
+            )
+        );
+        cloudFoundryCreds.create();
+    });
+
+    it("Perform CRUD Tests on Cloud Foundry Source platform", function () {
+        const platform = new SourcePlatform(
+            "CF-" + data.getRandomNumber(1, 200),
+            "Cloud Foundry",
+            Cypress.env("cloudfoundry_url"),
+            cloudFoundryCreds.name
+        );
+
+        platform.create();
+        checkSuccessAlert(
+            successAlertMessage,
+            `Success alert:Source platform ${platform.name} was successfully created.`,
+            true
+        );
+        exists(platform.name);
+
+        platform.delete();
+        notExists(platform.name);
+    });
+
+    after("Clear test data", function () {
+        cloudFoundryCreds.delete();
+    });
+});
