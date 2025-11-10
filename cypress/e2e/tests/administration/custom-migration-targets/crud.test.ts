078ae946175d92e5890a94a720537d86c58a9ab6
Maayan Hadasi <mguetta@redhat.com>
2025-11-10 10:01:49 +0200

    Making sure waitForTargets() runs on custom targets page
    Signed-off-by: Maayan Hadasi <mguetta@redhat.com>

--- cypress/e2e/tests/administration/custom-migration-targets/crud.test.ts
+++ cypress/e2e/tests/administration/custom-migration-targets/crud.test.ts
@@ -86,7 +86,7 @@ describe(["@tier0", "@interop"], "Custom Migration Targets CRUD operations", () 
                 cy.intercept("PUT", "/hub/targets*/*").as("putTarget");
                 cy.intercept("DELETE", "/hub/targets*/*").as("deleteTarget");
 
-                CustomMigrationTarget.open(true);
+                CustomMigrationTarget.open();
             });
 
             it("Custom Migration Targets CRUD with rules uploaded manually", function () {
