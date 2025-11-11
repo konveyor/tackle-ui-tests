0efc265d372e8b6f5043a9257758fd8bead51a8f
Nandini Chandra <nachandr@redhat.com>
2025-11-11 10:45:45 -0600

    8.0.0 Fix pre upgrade test (#1732)
    Signed-off-by: Nandini Chandra <nachandr@redhat.com>

--- cypress/e2e/views/rbac.view.ts
+++ cypress/e2e/views/rbac.view.ts
@@ -1,8 +1,8 @@
 export const addUserButton = "button[data-testid='add-user']";
 export const saveUserButton = "button[data-testid='user-creation-save']";
 export const createPasswordButton = "button[data-testid='no-credentials-empty-action']";
-export const passwordInput = "input[data-testid='passwordField']";
-export const passwordConfirm = "input[data-testid='passwordConfirmationField']";
+export const passwordInput = "#password";
+export const passwordConfirm = "#passwordConfirmation";
 export const kebabToogleButton = 'button[aria-label="Kebab toggle"]';
 export const modalConfirmButton = "#modal-confirm";
 export const tempPasswordToggle = "#temporaryPassword";
